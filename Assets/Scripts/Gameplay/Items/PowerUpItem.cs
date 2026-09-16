0dc56922330ffcce56ad56d16125881699a72601
AgustinLazaro <agustinblanco12@gmail.com>
2026-09-16 16:48:09 -0300

    refactor(powerup): delegate lifecycle and respawn logic to object pool
    - Removed internal respawn timer, sprite/collider caching, and random position logic
    
    - Set GameObject to deactivate on trigger enter to allow pool recycling
    - Maintained shield activation and particle hit spark triggering

--- Assets/Scripts/Gameplay/Items/PowerUpItem.cs
+++ Assets/Scripts/Gameplay/Items/PowerUpItem.cs
@@ -2,51 +2,20 @@ using UnityEngine;
 
 public class PowerUpItem : MonoBehaviour
 {
-    [SerializeField] private float _respawnTime = 5f;
     [SerializeField] private GameObject _shieldP1;
     [SerializeField] private GameObject _shieldP2;
     [SerializeField] private float _shieldDuration = 3f;
-
-    [Header("Player 1 X Range")]
-    [SerializeField] private float _minXP1 = -4f;
-    [SerializeField] private float _maxXP1 = -2f;
-
-    [Header("Player 2 X Range")]
-    [SerializeField] private float _minXP2 = 2f;
-    [SerializeField] private float _maxXP2 = 4f;
-
-    [Header("Y Range")]
-    [SerializeField] private float _minY = -2f;
-    [SerializeField] private float _maxY = 2f;
-
     [SerializeField] private ObjectPool _hitSparksPool;
-
-    private SpriteRenderer _spriteRenderer;
-    private Collider2D _collider;
-
-    private void Awake()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _collider = GetComponent<Collider2D>();
-    }
-
-    private void Start()
-    {
-        SetRandomPosition();
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent<Movement>(out Movement player))
         {
-            Debug.Log("Power up agarrado por: " + other.gameObject.name);
+            Debug.Log("Power up agarrado");
 
             ActivateShield(other.gameObject.name);
 
-            _spriteRenderer.enabled = false;
-            _collider.enabled = false;
-
-            Invoke(nameof(Respawn), _respawnTime);
+            gameObject.SetActive(false);
         }
     }
 
@@ -76,30 +45,5 @@ public class PowerUpItem : MonoBehaviour
     private void DeactivateShieldP2()
     {
         _shieldP2.SetActive(false);
-    }
-
-    private void Respawn()
-    {
-        SetRandomPosition();
-        _spriteRenderer.enabled = true;
-        _collider.enabled = true;
-    }
-
-    private void SetRandomPosition()
-    {
-        float positionX;
-
-        if (Random.Range(0, 2) == 0)
-        {
-            positionX = Random.Range(_minXP1, _maxXP1);
-        }
-        else
-        {
-            positionX = Random.Range(_minXP2, _maxXP2);
-        }
-
-        float positionY = Random.Range(_minY, _maxY);
-
-        transform.position = new Vector2(positionX, positionY);
     }
 }
